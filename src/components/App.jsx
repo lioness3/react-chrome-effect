c557109cb0db481a12542dc3aff3478aae0de6a5
lioness3 <jcnailtech3@yahoo.com>
2020-02-12 09:33:37 -0800

    add router config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
 
   return (
     <div>
     <Header/>
-    <TicketList/>
-    </div>
+    <Switch>
+      <Route exact path='/' component={TicketList} />
+    </Switch>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<HashRouter>
+                  <App />
+                </HashRouter>, document.getElementById('root'));
 serviceWorker.unregister();
